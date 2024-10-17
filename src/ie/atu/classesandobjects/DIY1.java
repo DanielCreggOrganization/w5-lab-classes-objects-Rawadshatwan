d2f8e5462d2e6dcd5a2afbc77ac5afa4a8b56d6c
rawadshatwan <145453561+Rawadshatwan@users.noreply.github.com>
2024-10-17 15:31:12 +0000

    can

---
+++ src/ie/atu/classesandobjects/DIY1.java
@@ -0,0 +1,12 @@
+package ie.atu.classesandobjects;
+
+public class DIY1 {
+    
+    String make;
+    String model;
+    int year;
+
+    public void startEngine() {
+      System.out.println("Engine started.");
+    }
+  }
